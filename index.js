0765c1fdfe9b0f82e5a59f6a69e28caecece9414
srivier <soleane.rivier@ynov.com>
2024-03-17 14:39:17 +0100

    fix : continue fixing error of merge

--- index.js
+++ index.js
@@ -100,7 +100,6 @@ function managementKeys(event) {
 
 const resetLevel = () => {
     grid = game.deepClone(game.cloneGrid)
-    console.log("pomme")
     player.resetPos(grid)
 }
 // Ajout d'un écouteur d'événements pour les touches du clavier
